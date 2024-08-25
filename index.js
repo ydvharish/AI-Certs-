ee218a29779c86f2710b56618d8fecdd9ffbde27
Harish Yadav <165556679+ydvharish@users.noreply.github.com>
2024-08-25 18:10:47 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+
+
+
+
+
+
+
+//javascript for navbar
+
+function showSidebar(){
+    const sidebar = document.querySelector
+    ('.sidebar')
+    sidebar.style.display = 'flex'
+}
+
+function hidesidebar(){
+    const sidebar = document.querySelector
+    ('.sidebar')
+    sidebar.style.display = 'none'
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
